aa517c0127d78bc5291bb5a72f4f939d3c4e1d80
Riocloud <tangr2016@gmail.com>
2023-04-21 05:05:43 -0400

    bug fix and new readme
    add logic

--- chatroom.js
+++ chatroom.js
@@ -5,9 +5,8 @@ class ChatroomScene extends Phaser.Scene {
 
   preload() {
     this.load.image("table", "assets/table.png");
-    this.load.image("avatar", "assets/avatar.png");
     this.load.image("speechBubble", "assets/speechBubble.png");
-    //Use the avatar upload from the login 
+
     const avatarDataUrl = localStorage.getItem('avatar');
     if (avatarDataUrl) {
       this.load.image('avatar', avatarDataUrl);
@@ -19,7 +18,7 @@ class ChatroomScene extends Phaser.Scene {
   create() {
     // Initialize WebSocket connection
     this.socket = new WebSocket("ws://your-websocket-server-url");
-    
+
     // WebSocket event listeners
     this.socket.addEventListener("open", (event) => {
       const username = localStorage.getItem("username");
@@ -30,48 +29,47 @@ class ChatroomScene extends Phaser.Scene {
     this.socket.addEventListener("message", (event) => {
       const message = JSON.parse(event.data);
 
-      if (message.type === "render") {
-        this.renderScene(message);
+      if (message.type === "update_state") {
+        this.renderScene(message.state);
       }
     });
-
-    // Render tables, avatars, and speech bubbles here
   }
 
-  renderScene(message) {
+  renderScene(state) {
     // Clear the existing scene
     this.children.removeAll();
 
     // Render tables
-    message.tables.forEach((table) => {
+    state.tables.forEach((table) => {
       renderTable(this, table.x, table.y);
     });
 
     // Render avatars
-    message.avatars.forEach((avatar) => {
-      renderAvatar(this, avatar.x, avatar.y, avatar.image);
+    state.users.forEach((user) => {
+      renderAvatar(this, user.x, user.y, user.avatar);
     });
 
     // Render speech bubbles
-    message.speechBubbles.forEach((speechBubble) => {
-      renderSpeechBubble(this, speechBubble.x, speechBubble.y, speechBubble.text);
+    state.messages.forEach((message) => {
+      const user = state.users.find(u => u.id === message.userId);
+      if (user) {
+        renderSpeechBubble(this, user.x, user.y - 50, message.text);
+      }
     });
   }
 }
 
-// Example function to render a table
 function renderTable(scene, x, y) {
   scene.add.image(x, y, "table");
 }
 
-// Example function to render an avatar
 function renderAvatar(scene, x, y, image) {
   scene.add.image(x, y, "avatar");
 }
 
-// Example function to render a speech bubble
 function renderSpeechBubble(scene, x, y, text) {
   const bubble = scene.add.image(x, y, "speechBubble");
   const textStyle = { fontSize: "16px", color: "#000", fontFamily: "Arial" };
   scene.add.text(x, y, text, textStyle).setOrigin(0.5);
 }
+
--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: "#f0f0f0",
-    scene: [ChatroomScene],
-  };
-  
-  const game = new Phaser.Game(config);
+import { createSpeechBubble } from './speechBubble.js';
+
+export class ChatroomScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'ChatroomScene' });
+  }
+
+  preload() {
+    this.load.image('table', 'assets/table.png');
+    this.load.image('speechBubble', 'assets/speechBubble.png');
+  }
+
+  create() {
+    const username = localStorage.getItem('username');
+    const avatarData = localStorage.getItem('avatar');
+    const avatar = new Image();
+    avatar.src = avatarData;
+
+    const table = this.add.image(400, 300, 'table');
+    table.setScale(0.5);
+
+    const person1 = this.add.sprite(250, 300, avatar);
+    person1.displayWidth = 128;
+    person1.displayHeight = 128;
+    person1.setInteractive();
+
+    const person2 = this.add.sprite(550, 300, avatar);
+    person2.displayWidth = 128;
+    person2.displayHeight = 128;
+    person2.setInteractive();
+
+    this.input.on('gameobjectdown', (pointer, gameObject) => {
+      const content = gameObject === person1
+        ? `Hello, I am ${username}`
+        : 'Hello, I am the AI';
+
+      const bubble = createSpeechBubble(this, gameObject.x, gameObject.y - 100, content);
+      this.time.delayedCall(3000, () => {
+        bubble.destroy();
+      });
+    });
+  }
+}
+
